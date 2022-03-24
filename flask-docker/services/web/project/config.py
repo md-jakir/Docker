f4598ba2b4398e41f77731fddaf7edc84c9c4132
Jakir Hossain <md-jakir@users.noreply.github.com>
2022-03-24 12:07:24 +0600

    Create config.py

---
+++ flask-docker/services/web/project/config.py
@@ -0,0 +1,11 @@
+config.py  __init__.py
+[root@dockerhost project]# cat config.py
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
